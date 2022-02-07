b9b187a198099e84fd1dc056e95391cd2005786d
Philippe Bogaerts <philippe.bogaerts@radarhack.com>
2022-02-07 15:58:13 +0100

    Create cilium_install.sh

---
+++ cilium_install.sh
@@ -0,0 +1,10 @@
+
+#!/bin/bash
+curl -L --remote-name-all https://github.com/cilium/cilium-cli/releases/latest/download/cilium-linux-amd64.tar.gz{,.sha256sum}
+sha256sum --check cilium-linux-amd64.tar.gz.sha256sum
+sudo tar xzvfC cilium-linux-amd64.tar.gz /usr/local/bin
+rm cilium-linux-amd64.tar.gz{,.sha256sum}
+cilium install
+cilium status --wait
+cilium connectivity test
+cilium hubble enable
