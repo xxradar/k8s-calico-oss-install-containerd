6affa393a1ac5422bb27dcbc43127be9fee58827
Philippe Bogaerts <philippe.bogaerts@radarhack.com>
2022-05-24 20:59:32 +0200

    Update setup_node_latest.sh

--- setup_node_latest.sh
+++ setup_node_latest.sh
@@ -39,6 +39,7 @@ sudo apt-get update && sudo apt-get install -y containerd
 # Configure containerd
 sudo mkdir -p /etc/containerd
 containerd config default | sudo tee /etc/containerd/config.toml
+sudo sed -i 's/#SystemdCgroup = false/SystemdCgroup = true/g' /etc/containerd/config.toml
 
 # Restart containerd
 sudo systemctl restart containerd
