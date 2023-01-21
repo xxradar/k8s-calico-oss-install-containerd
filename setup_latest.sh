16ab67055469fbb6848aca85f066e71e736a4e4c
Philippe Bogaerts <philippe.bogaerts@radarhack.com>
2023-01-21 13:49:46 +0100

    Update setup_latest.sh

--- setup_latest.sh
+++ setup_latest.sh
@@ -44,6 +44,7 @@ sudo apt-get install containerd.io
 # Configure containerd
 sudo mkdir -p /etc/containerd
 containerd config default | sudo tee /etc/containerd/config.toml
+sudo sed -i 's/SystemdCgroup = false/SystemdCgroup = true/g' /etc/containerd/config.toml
 
 # Restart containerd
 sudo systemctl restart containerd
