65508a0ed12271211b89221ded4fc0bf1c37e667
Philippe Bogaerts <philippe.bogaerts@radarhack.com>
2023-03-04 18:08:23 +0100

    Update calico_install.sh

--- calico_install.sh
+++ calico_install.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-curl https://docs.projectcalico.org/manifests/calico.yaml -O
+curl -L https://docs.projectcalico.org/manifests/calico.yaml -O
 
 sed -i 's/# - name: CALICO_IPV4POOL_CIDR/- name: CALICO_IPV4POOL_CIDR/g' calico.yaml
 sed -i 's/#   value: "192.168.0.0\/16"/  value: "192.168.0.0\/20"/g' calico.yaml
