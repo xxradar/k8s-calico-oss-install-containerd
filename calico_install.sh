7c48b564505644c1dfe103b034cd66038535598b
Philippe Bogaerts <philippe.bogaerts@radarhack.com>
2023-03-05 10:34:59 +0100

    Update calico_install.sh

--- calico_install.sh
+++ calico_install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/v3.24.5/manifests/tigera-operator.yaml
 curl https://raw.githubusercontent.com/projectcalico/calico/v3.24.5/manifests/custom-resources.yaml -O
-sed -i 's/encapsulation: VXLANCrossSubnet/encapsulation: Always/g' custom-resources.yaml
+sed -i 's/encapsulation: VXLANCrossSubnet/encapsulation: VXLAN/g' custom-resources.yaml
 kubectl apply -f ./custom-resources.yaml
 
