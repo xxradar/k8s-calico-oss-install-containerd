d27a95176dc692168d068993f21d05c757978877
Philippe Bogaerts <philippe.bogaerts@radarhack.com>
2023-08-16 15:26:48 +0200

    Update setup-cluster-config.sh

--- setup-cluster-config.sh
+++ setup-cluster-config.sh
@@ -89,4 +89,4 @@ sudo kubeadm init --config cluster.yaml
 mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
-kubectl taint nodes --all node-role.kubernetes.io/master- 
+kubectl taint nodes --all node-role.kubernetes.io/control-plane
