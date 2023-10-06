e1fa530f6e9bdc74c9a329a6ce522578c7d246d7
Philippe Bogaerts <philippe.bogaerts@radarhack.com>
2023-10-06 14:59:28 +0200

    Update setup_node_latest.sh

--- setup_node_latest.sh
+++ setup_node_latest.sh
@@ -28,6 +28,7 @@ cat <<EOF | sudo tee /etc/sysctl.d/99-kubernetes-cri.conf
 net.bridge.bridge-nf-call-iptables  = 1
 net.ipv4.ip_forward                 = 1
 net.bridge.bridge-nf-call-ip6tables = 1
+net.ipv6.conf.all.forwarding = 1
 EOF
 
 # Apply sysctl params without reboot
