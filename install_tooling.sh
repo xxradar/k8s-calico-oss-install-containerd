02aef42eeb1717297a9120b2ead94ba56751a4a4
Philippe Bogaerts <philippe.bogaerts@radarhack.com>
2023-10-27 15:44:26 +0200

    Update install_tooling.sh

--- install_tooling.sh
+++ install_tooling.sh
@@ -9,5 +9,6 @@ fi
 
 wget -q "https://github.com/containerd/nerdctl/releases/download/v${NERDCTL_VERSION}/nerdctl-full-${NERDCTL_VERSION}-linux-${archType}.tar.gz" -O /tmp/nerdctl.tar.gz
 tar -xzf /tmp/nerdctl.tar.gz --strip-components 1 bin/nerdctl
+chmod +x /tmp/bin/nerdctl
 sudo mv /tmp/bin/nerdctl /usr/local/bin/
 
