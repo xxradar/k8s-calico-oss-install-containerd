44372972bd4cd4e398646937139480b1b018739c
Philippe Bogaerts <philippe.bogaerts@radarhack.com>
2026-02-25 09:27:28 +0100

    Update Kubernetes repository to version 1.35

--- setup_latest.sh
+++ setup_latest.sh
@@ -7,8 +7,8 @@
 #sudo apt-add-repository "deb http://apt.kubernetes.io/ kubernetes-xenial main"
 
 sudo mkdir -p -m 755 /etc/apt/keyrings
-curl -fsSL  https://pkgs.k8s.io/core:/stable:/v1.34/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
-echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.34/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
+curl -fsSL  https://pkgs.k8s.io/core:/stable:/v1.35/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.35/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
 
 
 sudo apt update -y
